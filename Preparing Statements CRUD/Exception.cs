765057b59c7ec1e95a843cacb333145ab6b933b1
limaismael8901 <limaismael8901@gmail.com>
2023-11-26 05:35:46 -0300

    Adicionar arquivos de projeto.

---
+++ Preparing Statements CRUD/Exception.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Preparing_Statements_CRUD
+{
+    public class ExceptionText
+    {
+        public static void MetodoComExcecao(Exception ex)
+        {
+            MessageBox.Show("Error: " + ex.Message,
+            "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
---
+++ Preparing Statements CRUD/Form1.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Windows.Forms;
+
+
+
+namespace Preparing_Statements_CRUD
+{
+    public partial class Form1 : Form
+    {
+
+        MySqlConnection conn;
+        string sql = "datasource=localhost;username=root;password=123456;database=db_agenda";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Salvar_Click(object sender, EventArgs e)
+        {
+
+            if (!String.IsNullOrEmpty(txtNome.Text) &&
+                !String.IsNullOrEmpty(txtNome.Text) &&
+                !String.IsNullOrEmpty(txtNome.Text))
+            {
+                try
+                {
+                    conn = new MySqlConnection(sql);
+                    conn.Open();
+
+                    var comInsert = new MySqlCommand();
+
+                    comInsert.Connection = conn;
+
+                    comInsert.CommandText = "INSERT INTO contato (nome, email, telefone) "+
+                        "VALUES (@nome, @email, @telefone) ";
+
+                    comInsert.Parameters.AddWithValue("@nome", txtNome.Text);
+                    comInsert.Parameters.AddWithValue("@email", txtEmail.Text);
+                    comInsert.Parameters.AddWithValue("@telefone", txtTelefone.Text);
+
+                    comInsert.Prepare();
+
+                    comInsert.ExecuteNonQuery();
+
+                    MessageBox.Show("Deu tudo certo, contato adicionado!!");
+                }
+                catch (MySqlException ex)
+                {
+                    ExceptionText.MetodoComExcecao(ex);
+                }
+                catch (Exception ex)
+                {
+                    ExceptionText.MetodoComExcecao(ex);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Verifique se preencheu todos os campos.", "Error", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
